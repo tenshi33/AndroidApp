de3e3a4bf99d56a788e719806acd55e8749802af
tenshi33 <134793367+tenshi33@users.noreply.github.com>
2023-10-08 14:16:22 +0800

    Project

---
+++ app/src/main/java/com/example/myapplication/MainActivityLoginPage.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivityLoginPage extends AppCompatActivity {
+
+    private  Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_loginpage);
+
+        button = (Button) findViewById(R.id.loginbtn);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivityLoginPage.this, "LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                    openActivity2();
+                }else
+                    Toast.makeText(MainActivityLoginPage.this, "LOGIN FAILED",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivityMainPage.class);
+        startActivity(intent);
+    }
+}
