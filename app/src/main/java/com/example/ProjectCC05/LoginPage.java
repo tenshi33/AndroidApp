7b5f0d5652a88f734f2ab8b82eda75a32f11bb77
tenshi33 <134793367+tenshi33@users.noreply.github.com>
2023-11-08 01:27:06 +0800

    Test ang panget ko

--- app/src/main/java/com/example/ProjectCC05/LoginPage.java
+++ app/src/main/java/com/example/ProjectCC05/LoginPage.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class LoginPage extends AppCompatActivity {
 
     private Button button;
-
+//ang panget ko
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
