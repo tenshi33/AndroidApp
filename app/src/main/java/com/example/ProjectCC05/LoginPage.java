8feb0c18c3335add53caf5522635cbdcc995be79
tenshi33 <134793367+tenshi33@users.noreply.github.com>
2023-11-07 23:47:20 +0800

    SignUp

--- app/src/main/java/com/example/ProjectCC05/LoginPage.java
+++ app/src/main/java/com/example/ProjectCC05/LoginPage.java
@@ -10,36 +10,59 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
+
     private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        button = (Button)findViewById(R.id.loginbtn);
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
-        Button loginbtn = (Button) findViewById(R.id.loginbtn);
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+
+        TextView clickableTextView = findViewById(R.id.signup);
+
+        clickableTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (username.getText().toString().equals("admin")&& password.getText().toString().equals("1234")) {
+                openSignUpPage();
+            }
+        });
+
+        button = findViewById(R.id.loginbtn);
+        final TextView username = findViewById(R.id.username);
+        final TextView password = findViewById(R.id.password);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredUsername = username.getText().toString();
+                String enteredPassword = password.getText().toString();
+
+                if (enteredUsername.equals("admin") && enteredPassword.equals("1234")) {
                     Toast.makeText(LoginPage.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
                     openActivity2();
-                }else
-                    Toast.makeText(LoginPage.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
-
+                } else {
+                    Toast.makeText(LoginPage.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
                 }
+            }
         });
     }
-    public void openActivity2(){
-        Intent intent = new Intent(this,MainPage.class);
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainPage.class);
         startActivity(intent);
     }
-    public void onBackPressed(){
-        openMainActivity();
 
+    public void onBackPressed() {
+        openMainActivity();
     }
-    public void openMainActivity(){
+
+    public void openMainActivity() {
         Intent intent = new Intent(this, MainActivityLoginPage.class);
+        startActivity(intent);
+    }
+
+    public void openSignUpPage() {
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ProjectCC05/SignUpPage.java
+++ app/src/main/java/com/example/ProjectCC05/SignUpPage.java
@@ -1,8 +1,12 @@
 package com.example.ProjectCC05;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpPage extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        TextView clickableTextView = findViewById(R.id.signin);
+        clickableTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignInPage();
+            }
+        });
+    }
+
+    public void openSignInPage() {
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
     }
 }
