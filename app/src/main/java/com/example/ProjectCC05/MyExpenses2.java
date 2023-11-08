06a5bb6dd782a0911e65141bf148914eefa49e52
Bersalus <kurtlaurencetoledo@gmail.com>
2023-11-08 17:49:32 +0800

    Removed Initialized Items in the History Page

--- app/src/main/java/com/example/ProjectCC05/MyExpenses2.java
+++ app/src/main/java/com/example/ProjectCC05/MyExpenses2.java
@@ -15,17 +15,18 @@ public class MyExpenses2{
     }
 
     public MyExpenses2(){
-        String[] startingExpenses = {"Water Bill", "Electric Bill", "Tuition"};
-        String[] startingDate = {"01/02/2024", "01/03/2024", "01/04/2024"};
+        //String[] startingExpenses = {"Water Bill", "Electric Bill", "Tuition"};
+        //String[] startingDate = {"01/02/2024", "01/03/2024", "01/04/2024"};
 
         this.myExpenseList2 = new ArrayList<>();
 
+        /*
         Random rand = new Random();
         for (int i = 0; i<startingExpenses.length; i++){
-            Expense2 p = new Expense2(startingExpenses[i], rand.nextInt(1500) + 100, /*rand.nextInt(50),*/ startingDate[i]);
+            Expense2 p = new Expense2(startingExpenses[i], rand.nextInt(1500) + 100, /*rand.nextInt(50),/ startingDate[i]);
             myExpenseList2.add(p);
         }
-
+        */
     }
 
     public List<Expense2> getMyExpenseList2() {
